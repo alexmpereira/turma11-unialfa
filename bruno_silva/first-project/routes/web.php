1e8a2b5a623b70a4e4c1a5c1f094d18cc9fa2656
Bruno Silva <bruno.sil16441@gmail.com>
2022-02-24 22:23:54 -0300

    feat: add new route client

--- bruno_silva/first-project/routes/web.php
+++ bruno_silva/first-project/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ClientController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/clients', [ClientController::class, 'index']);
