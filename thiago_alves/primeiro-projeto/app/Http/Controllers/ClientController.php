49b93a6f8fc18f4536b29a503ed5adb9c13f410e
Thiago Alves <thiaguinho2308@gmail.com>
2022-03-03 19:39:50 -0300

    ClientController

---
+++ thiago_alves/primeiro-projeto/app/Http/Controllers/ClientController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    public function index()
+    {
+        return 'Deu bom';
+    }
+}
