287c66298d0911f264fc1eba9fa2bf5f11de4c89
lucas mendes <lucas02.mendes@gmail.com>
2022-03-10 22:11:35 -0300

    criadas duas novas view

---
+++ lucas_mendes/primeiro-projeto/resources/views/clients/index.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Clientes</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Clientes</a>
+        </div>
+    </nav>
+
+    <div class="container">
+        <h1>Lista de clientes</h1>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Endereco</th>
+      <th scope="col">Ações</th>
+    </tr>
+  </thead>
+  <tbody>
+      @foreach ($clients as $client)
+        <tr>
+        <th scope="row">{{ $client->id }}</th>
+        <td>
+            <a href="{{route('clients.show', $client)}}">
+            {{ $client->nome }}
+            </a>
+        </td>
+        <td>{{ $client->endereco }}</td>
+        </tr>
+     @endforeach
+  </tbody>
+</table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
