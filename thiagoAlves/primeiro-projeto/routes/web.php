14d437f1371b71c42e82c6a7e9a961ecb0e31b42
Thiago Alves <thiaguinho2308@gmail.com>
2022-03-10 22:02:20 -0300

    Criação das telas Listagem e Detalhes
    - Aqui aprendemos a colocar nomes para nossas rotas
    - Aprendemos a separar os arquivos blade com os métodos

--- thiagoAlves/primeiro-projeto/routes/web.php
+++ thiagoAlves/primeiro-projeto/routes/web.php
@@ -18,4 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/clients', [ClientController::class, 'index']);
+Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
+Route::get('/client/{id}', [ClientController::class, 'show'])->name('clients.show');
+
+Route::get('/clients/{id}', [ClientController::class, 'destroy']);
