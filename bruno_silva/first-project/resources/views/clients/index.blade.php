882ce7b128a639444056e0f85e26ef23e7041a0c
Bruno Silva <bruno.sil16441@gmail.com>
2022-03-17 19:43:59 -0300

    feat: add new features

--- bruno_silva/first-project/resources/views/clients/index.blade.php
+++ bruno_silva/first-project/resources/views/clients/index.blade.php
@@ -1,23 +1,7 @@
-<!doctype html>
-<html lang="en">
+@extends('app')
+@section('title', 'lista de clientes')
+@section('content')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Lista de clientes</title>
-</head>
-
-<body>
-    <nav class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1">Unialfa</span>
-        </div>
-    </nav>
 
     <div class="container">
         <h1>Lista de clientes:</h1>
@@ -31,19 +15,17 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach($clients as $client)
-                <tr>
-                    <th scope="row">{{$client->id}}</th>
-                    <td>
-                        <a href="{{ route('clients.show', $client) }}">{{$client->nome}}</a>
-                    </td>
-                    <td>{{$client->endereco}}</td>
-                </tr>
+                @foreach ($clients as $client)
+                    <tr>
+                        <th scope="row">{{ $client->id }}</th>
+                        <td>
+                            <a href="{{ route('clients.show', $client) }}">{{ $client->nome }}</a>
+                        </td>
+                        <td>{{ $client->endereco }}</td>
+                    </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
 
-</html>
+@endsection
