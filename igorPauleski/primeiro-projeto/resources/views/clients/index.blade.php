3bf00ac8bafbc79de3c778c3e3783fd26ba5a0bf
Igor Pauleski <igorpauleski@hotmail.com>
2022-03-17 19:43:27 -0300

    MElhoramos utilizando o Blade e limpeza de código

--- igorPauleski/primeiro-projeto/resources/views/clients/index.blade.php
+++ igorPauleski/primeiro-projeto/resources/views/clients/index.blade.php
@@ -1,48 +1,29 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-        <title>Hello, world!</title>
-    </head>
-    <body>
-    <nav class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">UniALFA</a>
-        </div>
-    </nav>
-        <div>
-            <h1>Lista de Cliente</h1>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Endereço</th>
-                        <th scope="col">Ações</th>
-                    </tr>
-                </thead>
-            </table>
-            <tbody>
-                @foreach ($clients as $client)
-                    <tr>
-                        <th space="row"> {{ $client->id }} </th>
-                        <td>
-                            <a href="{{ route('clients.show', $client)}}">
-                                {{ $client->nome }}
-                            </a>
-                        </td>
-                        <td> {{ $client->endereço }} </td>
-                        <td>@mdo</td>
-                    </tr>
-                @endForeach
-            </tbody>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></>
-    </body>
-</html>
+@extends('app')
+@section('title', 'Lista de clientes')
+@section('content')
+    <h1>Lista de Cliente</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Endereço</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+    <tbody>
+        @foreach ($clients as $client)
+            <tr>
+                <th space="row"> {{ $client->id }} </th>
+                <td>
+                    <a href="{{ route('clients.show', $client)}}">
+                        {{ $client->nome }}
+                    </a>
+                </td>
+                <td> {{ $client->endereço }} </td>
+                <td>@mdo</td>
+            </tr>
+        @endforeach
+    </tbody>
+    </table>
+@endsection
