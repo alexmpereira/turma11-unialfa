e4764fab3f27be81c80c4ba127c22d2738882e34
Robson Melo <melodisouza@gmail.com>
2022-03-17 19:43:38 -0300

    Melhoria do código utilizando template do Blade

--- robson_melo/primeiro-projeto/resources/views/clients/index.blade.php
+++ robson_melo/primeiro-projeto/resources/views/clients/index.blade.php
@@ -1,26 +1,7 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Lista de Clientes</title>
-</head>
-
-<body>
-    <nav class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Unialfa</a>
-        </div>
-    </nav>
-
-    <div class="container">
-        <h1 class="text-center">Lista de Clientes</h1>
+@extends('app')
+@section('title', 'Lista de Clientes')
+@section('content')
+  <h1>Lista de Clientes</h1>
         <table class="table">
             <thead>
                 <tr>
@@ -43,12 +24,4 @@
                 @endforeach
             </tbody>
         </table>
-
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-
-</body>
-
-</html>
+@endsection
