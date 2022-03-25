72ba0e3dd2f9f6e6af12b54dcf6bb1ee1651f1c7
BrN-Gabs <brunogabriel58@outlook.com>
2022-03-24 21:09:00 -0300

    Finalização do projeto com a criação do edit e delete dos clientes

--- brunogabriel/primeiro-projeto/resources/views/clients/index.blade.php
+++ brunogabriel/primeiro-projeto/resources/views/clients/index.blade.php
@@ -22,7 +22,23 @@
                             </a>
                         </td>
                         <td>{{ $client->endereco }}</td>
-                        <td></td>
+                        <td>
+                            <a class="btn btn-primary" href="{{route('clients.edit', $client)}}">
+                                Editar
+                            </a> 
+                            
+                            <form action="{{route('clients.destroy', $client)}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button 
+                                    class="btn btn-danger" 
+                                    type="submit"
+                                    onclick="return confirm('Tem certeza que deseja apagar?')"
+                                >
+                                    Apagar
+                                </button>
+                            </form>    
+                        </td>
                 </tr>
                 @endforeach
             </tbody>
