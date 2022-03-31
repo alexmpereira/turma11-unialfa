a8f45b98c7cd6ba02ffeacd73bd4590ac3b00b8f
lucas mendes <lucas02.mendes@gmail.com>
2022-03-31 20:52:23 -0300

    desafio 1 e 2 concluidos

---
+++ lucas_mendes/desafios/resources/views/estudantes/index.blade.php
@@ -0,0 +1,48 @@
+@extends('app')
+@section('title', 'Lista de estudantes')
+
+@section('content')
+  <h1>Lista de Estudantes</h1>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Nome</th>
+        <th scope="col">cpf</th>
+        <th scope="col">nascimento</th>
+        <th scope="col">Sala</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($estudantes as $estudante)
+        <tr>
+          <th scope="row">{{ $estudante->id }}</th>
+            <td>
+                <a href="{{route('estudantes.show', $estudante)}}">
+                {{ $estudante->nome }}
+                </a>
+            </td>
+          <td>{{ $estudante->cpf }}</td>
+          <td>{{ $estudante->nascimento }}</td>
+          <td>{{ $estudante->sala_id }}</td>
+          <td>
+            <a class="btn btn-primary" href="{{route('estudantes.edit', $estudante)}}">
+              Editar
+            </a>
+            <form action="{{route('estudantes.destroy', $estudante)}}" method="POST">
+              @csrf
+              @method('DELETE')
+              <button class="btn btn-danger" type="submit" onclick="return confirm('Tem certeza que deseja apagar?')">
+                Deletar
+              </button>
+            </form>
+          </td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+
+  <a class="btn btn-success" href="{{ route('estudantes.create') }}">
+    Novo Estudante  
+  </a>
+@endsection
