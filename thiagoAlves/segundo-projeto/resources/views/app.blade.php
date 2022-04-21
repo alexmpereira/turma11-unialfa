e98811a657a012a427bb4450c36cef9d85797d86
ThiaAlves <thiaguinho2308@gmail.com>
2022-04-21 19:53:11 -0300

    Finalização de Trabalho (Desafio 2)

--- thiagoAlves/segundo-projeto/resources/views/app.blade.php
+++ thiagoAlves/segundo-projeto/resources/views/app.blade.php
@@ -8,19 +8,27 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <title>@yield('title')</title>
   </head>
-  <body style="background-color: rgb(230, 245, 246)">
-  <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-lg">
+  <body style="background: rgb(54,203,199);
+  background: linear-gradient(63deg, rgba(54,203,199,1) 14%, rgba(96,222,110,1) 62%, rgba(125,236,49,1) 100%);">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="{{ route('estudantes.index') }}"><span class="lnr lnr-smile"></span> Unialfa</a>
+    <a class="navbar-brand" href="{{ route('series.index') }}"><img src="https://www.alfaumuarama.edu.br/fau/images/logop.png?v=1" width="50%"></a>
   </div>
     </nav>
 
-    <div class="container p-5 shadow-lg p-3 mb-5 bg-white rounded mt-5">
+    <div class="container">
         @yield('content')
     </div>
+
+
+    <footer class="footer bg-light fixed-bottom p-4 shadow-lg">
+        <p class="text-center m-2">© Copyright - {{ date("Y") }} Desenvolvido por <a href="https://github.com.bt/thiaalves">thiago.alves</a></p>
+    </footer>
+
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.linearicons.com/free/1.0.0/svgembedder.min.js"></script>
---
+++ thiagoAlves/segundo-projeto/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SerieController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/series', [SerieController::class, 'index'])->name('series.index');
+Route::get('/series/create', [SerieController::class, 'create'])->name('series.create');
+
+Route::post('/series', [SerieController::class, 'store'])->name('series.store');
