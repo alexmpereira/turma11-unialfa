36767b112f7165a3394fe4e28c6411b0fada3eda
Lucas Pechebelok <lucaspechebelok@gmail.com>
2022-04-21 18:52:31 -0300

    desafio 1 e 2

--- lucas_pechebelok/desafios/database/migrations/2022_03_31_233735_update_students_table.php
+++ lucas_pechebelok/desafios/database/migrations/2022_03_31_233735_update_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStudentsTable extends Migration
+class UpdateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome');
-            $table->string('cpf');
-            $table->date('nascimento');
-            $table->timestamps();
+        Schema::table('students', function (Blueprint $table) {
+            //$table->string('sala_id', 200);
         });
     }
 
@@ -29,6 +25,8 @@ class CreateStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::table('students', function (Blueprint $table) {
+            //
+        });
     }
-};
+}
--- lucas_pechebelok/desafios/routes/web.php
+++ lucas_pechebelok/desafios/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstudanteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $dados = ['id' => 2, 'name' => 'Igor'];
-    $dados = json_encode($dados, true);
-    dd($dados);exit;
+    return view('welcome');
 });
+
+Route::get('/students', [EstudanteController::class, 'index'])->name('students.index');
+Route::get('/students/create', [EstudanteController::class, 'create'])->name('students.create');
+Route::get('/students/{id}', [EstudanteController::class, 'show'])->name('students.show');
+Route::get('/students/{id}/edit', [EstudanteController::class, 'edit'])->name('students.edit');
+
+Route::post('/students', [EstudanteController::class, 'store'])->name('students.store');
+Route::put('/students/{id}', [EstudanteController::class, 'update'])->name('students.update');
+Route::delete('/students/{id}', [EstudanteController::class, 'destroy'])->name('students.destroy');
