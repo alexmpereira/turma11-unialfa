6b26b01d95fbc29ca7a3f0b2941e2ca45cd81e19
ThiaAlves <thiaguinho2308@gmail.com>
2022-05-12 20:08:00 -0300

    Criação de Tabelas e Chave estrangeira

---
+++ thiagoAlves/segundo-projeto/database/migrations/2022_05_12_222100_create_temporadas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temporadas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('numero');
+
+            //Nessa tabela, o campo serie_id é um campo de relacionamento
+            $table->unsignedBigInteger('serie_id');
+            $table->foreign('serie_id')->references('id')->on('series');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temporadas');
+    }
+};
