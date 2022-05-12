eb16d4dd169629265690d1e3517e16a9de29a302
ViniciosG <viniciossantanag@gmail.com>
2022-05-12 20:31:44 -0300

    table temporadas e relacioanmentos de tabelas

---
+++ vinicios_santana/segundo_projeto/app/Models/Episodio.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Episodio extends Model
+{
+    use HasFactory;
+
+    public function temporada()
+    {
+        return $this->belongsTo(Temporada::class);
+    }
+}
