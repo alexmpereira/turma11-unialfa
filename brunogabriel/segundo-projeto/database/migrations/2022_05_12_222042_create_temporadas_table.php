d74a1065f0185f952c8e1b6fd5840e97f2958365
BrN-Gabs <brunogabriel58@outlook.com>
2022-05-12 20:16:17 -0300

    fazendo as ligacoes das tabelas

---
+++ brunogabriel/segundo-projeto/database/migrations/2022_05_12_222042_create_temporadas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTemporadasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temporadas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('numero');
+
+            /*
+             * Nessa tabela vai conter uma coluna serie_id que irá ser uma FK
+             * de série. No caso serie_id faz referencia ao id da tabela series
+             */
+            $table->unsignedBigInteger('serie_id');
+            $table->foreign('serie_id')->references('id')->on('series');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temporadas');
+    }
+};
