d80bba449085b5079d93ac94108b71b057525cea
FelipeOtavio <felipecheronn@gmail.com>
2022-05-12 20:07:32 -0300

    adicionando tabelas

--- felipe_otavio/segundo-projeto/database/migrations/2022_05_12_223025_create_episodios_table.php
+++ felipe_otavio/segundo-projeto/database/migrations/2022_05_12_223025_create_episodios_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('episodios', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->timestamps();
+            $table->integer('numero');
+
+            $table->unsignedBigInteger('temporada_id');
+            $table->foreign('temporada_id')->references('id')->on('temporadas');
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('episodios');
     }
 };
