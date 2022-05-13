4ebd987e91b3d35421886de8111fda6f4049ca6b
username <user@mail.com>
2022-05-12 23:07:38 -0300

    cria relacionamento entre tabelas serie/temporada/episodio

---
+++ evandroQuirino/segundo-projeto/app/Models/Temporada.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Temporada extends Model
+{
+    use HasFactory;
+
+    public function episodios()
+    {
+        return $this->hasMany(Episodio::class);
+    }
+
+    public function serie()
+    {
+        return $this->belongsTo(Serie::class);
+    }
+}
---
+++ evandroQuirino/segundo-projeto/database/migrations/2022_05_13_013122_create_episodios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodios', function (Blueprint $table) {
+            $table->id();
+            $table->integer('numero');
+
+            $table->unsignedBigInteger('temporada_id');
+            $table->foreign('temporada_id')->references('id')->on('temporadas');
+            
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodios');
+    }
+};
