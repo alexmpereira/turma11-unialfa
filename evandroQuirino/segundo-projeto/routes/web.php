ced1b83c1963be28076c4d7e9e8fe9bf075fad31
evandroquirino <evandroquirin@gmail.com>
2022-05-12 21:29:48 -0300

    Revert "cria funcao de listagem e create"
    This reverts commit 84cfce8f0663bc224a939e014add7d3db27154b3.

--- evandroQuirino/segundo-projeto/routes/web.php
+++ evandroQuirino/segundo-projeto/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/series', [SeriesController::class, 'index'])->name('series.index');
-Route::get('/series/create', [SeriesController::class, 'create'])->name('series.create');
-
-Route::post('/series', [SeriesController::class, 'store'])->name('series.store');
-
-
-
+Route::get('/', function () {
+    return view('welcome');
+});
