4f6f535abe7377a9abd9b66d67d368ebfc6f34bb
Robson Melo <melodisouza@gmail.com>
2022-05-19 19:53:22 -0300

    Refatoracao com criacao de service.

---
+++ robson_melo/segundo-projeto/app/Services/CriarSerie.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Serie as SerieModel;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class CriarSerie
+{
+    public function save(
+        string $nomeSerie,
+        int $qtdTemporadas,
+        int $epPorTemporada
+    ): SerieModel{
+        DB::beginTransaction();
+        $serie = SerieModel::create(['name'=> $nomeSerie]);
+        $this->criaTemporadas($qtdTemporadas, $epPorTemporada, $serie);
+        DB::commit();
+
+        return $serie;
+    }
+
+    public function criaTemporadas(
+        int $qtdTemporadas,
+        int $epPorTemporada,
+         SerieModel $serie): void {
+        for($i=1; $i<= $qtdTemporadas; $i++){
+            $temporada =$serie->temporadas()->create(['numero'=> $i]);
+
+            $this->criaEpisodios($epPorTemporada, $temporada);
+        }
+    }
+
+    public function criaEpisodios(
+        int $epPorTemporada,
+         Model $temporada): void {
+        for($j=1; $j<= $epPorTemporada; $j++){
+            $temporada->episodios()->create(['numero'=>$j]);
+        }
+    }
+}
