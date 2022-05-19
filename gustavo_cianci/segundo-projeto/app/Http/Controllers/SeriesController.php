ee2d729dfc6f1a7452bd8b0d5788f1d76fb2c38d
Gu18071 <gus.cianci18@hotmail.com>
2022-05-19 19:56:07 -0300

    dinamica

--- gustavo_cianci/segundo-projeto/app/Http/Controllers/SeriesController.php
+++ gustavo_cianci/segundo-projeto/app/Http/Controllers/SeriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Serie;
+use App\Services\CriarSerie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
@@ -39,19 +40,17 @@ class SeriesController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(SeriesFormRequest $request)
+    public function store(SeriesFormRequest $request , CriarSerie $criarSerie)
     {
 
        
-        $serie = Serie::create(['nome' => $request->nome]);
-        $qtdTemporadas = $request->qtd_temporadas;
-        for($i=1; $i <= $qtdTemporadas; $i++ ){
-            $temporada = $serie->temporadas()->create(['numero' => $i]);
-
-            for($j = 1; $j <= $request->ep_por_temporada; $j++){
-                $temporada->episodios()->create(['numero'=> $j]);
-            }
-        }
+      
+      
+        $serie = $criarSerie->save(
+            $request->nome,
+            $request->qtd_temporadas,
+            $request->ep_por_temporada
+        );
         $request->session()->flash("mensagem", "Série {$serie->id}e suas temporadas e episodios criados criados com sucesso {$serie->nome}");
         return redirect()->route('series.index');
 
