57a270d27923df0c791dc802be1c0720f32b1c34
Alarcon <pedrohenriquealarcon@gmail.com>
2022-05-19 19:58:28 -0300

    Refatorando

--- Pedro_H_Alarcon/segundo-projeto/database/factories/UserFactory.php
+++ Pedro_H_Alarcon/segundo-projeto/database/factories/UserFactory.php
@@ -5,15 +5,12 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
     public function definition()
     {
@@ -29,7 +26,7 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      *
-     * @return static
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
     public function unverified()
     {
--- Pedro_H_Alarcon/segundo-projeto/database/migrations/2022_05_12_223027_create_episodios_table.php
+++ Pedro_H_Alarcon/segundo-projeto/database/migrations/2022_05_12_223027_create_episodios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('episodios', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->integer('numero');
+            
+            // o campo temporada_id faz referencia ao campo id da tabela temporada
+            $table->unsignedBigInteger('temporada_id');
+            $table->foreign('temporada_id')->references('id')->on('temporadas');
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('episodios');
     }
-}
+};
