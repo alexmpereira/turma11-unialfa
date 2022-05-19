9fdb4bb0b886a743039643ee78e099401ef2b13e
Eduardo Scarante <eduardo.scarante543@gmail.com>
2022-05-19 20:03:14 -0300

    add rotas e services

--- eduardo_scarante/segundo-projeto/routes/web.php
+++ eduardo_scarante/segundo-projeto/routes/web.php
@@ -1,8 +1,19 @@
 <?php
 
 use App\Http\Controllers\SeriesController;
+use App\Http\Controllers\TemporadasController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,5 +22,7 @@ Route::get('/', function () {
 
 Route::get('/series',[SeriesController::class, 'index'])->name('series.index');
 Route::get('series/create',[SeriesController::class,'create'])->name('series.create');
+Route::get('series/{id}/temporadas', [TemporadasController::class, 'index'])->name('temporadas.index');
 
 Route::post('series', [SeriesController::class,'store'])->name('series.store');
+Route::delete('/series/{id}',[SeriesController::class, 'destroy'])->name('series.destroy');
