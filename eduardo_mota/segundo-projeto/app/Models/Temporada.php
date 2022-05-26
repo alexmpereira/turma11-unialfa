b8812f7fb28c312ef97089833fd66af0bf740e79
Dumota <edumota02gmail.com>
2022-05-26 20:07:54 -0300

    adicionando check de episodios

--- eduardo_mota/segundo-projeto/app/Models/Temporada.php
+++ eduardo_mota/segundo-projeto/app/Models/Temporada.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -32,5 +33,12 @@ class Temporada extends Model
     public function serie()
     {
         return $this->belongsTo(Serie::class);
+    }
+
+    public function getEpisodiosAssistido(): Collection
+    {
+        return $this->episodios->filter(function(Episodio $episodio){
+            return $episodio->assistido;
+        });
     }
 }
--- eduardo_mota/segundo-projeto/routes/web.php
+++ eduardo_mota/segundo-projeto/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EpisodiosController;
 use App\Http\Controllers\SeriesController;
 use App\Http\Controllers\TemporadasController;
 use Illuminate\Support\Facades\Route;
@@ -24,8 +25,12 @@ Route::get('/series',[SeriesController::class, 'index'])->name('series.index');
 Route::get('series/create',[SeriesController::class,'create'])->name('series.create');
 Route::get('series/{id}/temporadas', [TemporadasController::class, 'index'])->name('temporadas.index');
 
+
 Route::post('series', [SeriesController::class,'store'])->name('series.store');
 
 Route::post('/series/{id}/editaNome', [SeriesController::class,'editaNome']);
 
 Route::delete('/series/{id}',[SeriesController::class, 'destroy'])->name('series.destroy');
+
+Route::get('/temporadas/{temporada}/episodios',[EpisodiosController::class , 'index']);
+Route::post('/temporadas/{temporada}/episodios/assistir', [EpisodiosController::class,'assistir']);
