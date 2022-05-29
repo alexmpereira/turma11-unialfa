8f05eb311bad52a9c2fc0da4b039181da21a7f55
FelipeOtavio <felipecheronn@gmail.com>
2022-05-29 12:44:37 -0300

    trabalho-prova

---
+++ felipe_otavio/trabalho/app/Http/Controllers/FiliaisController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Filiais;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+
+class FiliaisController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return View
+     */
+    public function index()
+    {
+        $filiais = Filiais::get();
+        return view(
+            'filiais.index',['filiais' => $filiais]
+        );
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return View
+     */
+    public function show(int $id)
+    {
+        $filiais = Filiais::find($id);
+        return view(
+            'filiais.show',['filiais' => $filiais]
+        );
+    }
+}
---
+++ felipe_otavio/trabalho/app/Models/Filiais.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Filiais extends Model
+
+
+{
+    protected $fillable = [
+        'nome_filial',
+        'regiao',
+        'data_criacao'
+    ];
+    use HasFactory;
+}
