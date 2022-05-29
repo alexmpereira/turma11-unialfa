b9a27214b692cef0d199d17541b8e7088a5721e1
Robson Melo <melodisouza@gmail.com>
2022-05-29 07:13:37 -0300

    Trabalho01_2bim_recriacaoAvaliacaoRobsonMelo

---
+++ robson_melo/avaliacao/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FuncionariosController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/funcionarios',[FuncionariosController::class, 'index'])->name('funcionarios.index');
+
+Route::get('/funcionarios/{id}', [FuncionariosController::class, 'show'])->name('funcionarios.show');
