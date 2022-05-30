362382fad81d21da2b6e8f6647af98957dbafce0
Gu18071 <gus.cianci18@hotmail.com>
2022-05-30 00:15:40 -0300

    Trabalho 1 - prova

---
+++ gustavo_cianci/Avaliacao/database/migrations/2022_05_29_094106_create_funcionarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFuncionariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->string('cargo', 100);
+            $table->float('salario', 50);
+            $table->date('data_entrada');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionarios');
+    }
+}
