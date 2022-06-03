18bf8e5aba9d0c197bbec06092116dd9a8209bf3
ThiaAlves <thiaguinho2308@gmail.com>
2022-06-02 21:25:42 -0300

    Projeto Pokemon Adicionado

---
+++ thiagoAlves/api-app/app/Http/Controllers/BattleController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BattleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($poke1, $poke2)
+    {
+
+        $firstPokemon = $this->getDataPokemon($poke1);
+        $firstPokemonAttack = $this->getAttack($poke1,
+            $firstPokemon['stats']);
+
+        $secondPokemon = $this->getDataPokemon($poke2);
+        $secondPokemonAttack = $this->getAttack($poke2,
+            $secondPokemon['stats']);
+
+            $pokemonsMerge = array_merge($firstPokemon, $secondPokemon);
+            $battle = array_search(max($pokemonsMerge), $pokemonsMerge);
+
+            echo strtoupper($battle) . " - Venceu!" .PHP_EOL;
+            exit;
+            return true;
+
+
+
+    }
+
+    public function getDataPokemon(string $namePokemon) {
+        $client = new \GuzzleHttp\Client();
+        $namePokemon = $client->request('GET', 'https://pokeapi.co/api/v2/pokemon/'.$namePokemon.'/');
+        return json_decode($namePokemon->getBody()->getContents() , true);
+    }
+
+    public function getAttack(string $nomePokemon, array $stats):array{
+
+        $recuperaValorAttack = 0;
+        foreach ($stats as $key => $value) {
+            $recuperaValorAttack += $value['base_stat'];
+        }
+
+        return ["$nomePokemon" => $recuperaValorAttack];
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
