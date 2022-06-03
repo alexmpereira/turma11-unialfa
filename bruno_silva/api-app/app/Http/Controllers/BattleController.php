97be0caeb1c40a81f5a7ef12e0db4d0dcada42bd
Bruno Silva <bruno.sil16441@gmail.com>
2022-06-02 21:51:36 -0300

    Add logs on request and change route name

--- bruno_silva/api-app/app/Http/Controllers/BattleController.php
+++ bruno_silva/api-app/app/Http/Controllers/BattleController.php
@@ -2,48 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class BattleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($poke1, $poke2)
+    public function startBattle($poke1, $poke2)
     {
         $firstPoke = $this->getDataPoke($poke1);
         $firstPokeAttack = $this->getAttack($poke1, $firstPoke['stats']);
@@ -51,21 +16,53 @@ class BattleController extends Controller
         $secondPoke = $this->getDataPoke($poke2);
         $secondPokeAttack = $this->getAttack($poke2, $secondPoke['stats']);
         $pokeMerge = array_merge($secondPokeAttack, $firstPokeAttack);
-        $battle = array_search(max($pokeMerge), $pokeMerge);
 
-        echo strtoupper($battle) . ' Ganhou a Batalha';
+        Log::info('Batalha Efetuada entre $poke1 e $poke2', $pokeMerge);
+        return Response(
+            $this->battle($poke1, $poke2, $pokeMerge)
+        );
     }
 
-public function getAttack(string $nomePoke, array $stats)
-{
-    $baseStatsSum = 0;
 
-    foreach ($stats as $key => $stat) {
-        $baseStatsSum += $stat['base_stat'];
+    public function battle(string $poke1, string $poke2, $pokes)
+    {
+        if ($pokes[$poke1] > $pokes[$poke2]) {
+            return $this->makeArrayWinner($poke1);
+        }
+
+
+        if ($pokes[$poke2] > $pokes[$poke1]) {
+            return $this->makeArrayWinner($poke2);
+        }
+
+        Log::error('Ocorreu um empate', $pokes);
+
+
+        return $this->makeArrayDefault();
     }
 
-    return[$nomePoke => $baseStatsSum];
-}
+    public function makeArrayWinner(string $pokemon)
+    {
+        return [
+            'winner' => strtoupper($pokemon) . ' Ganhou a Batalha'
+        ];
+    }
+
+    public function makeArrayDefault()
+    {
+        return ['status' => 'Ocorreu um empate'];
+    }
+
+    public function getAttack(string $nomePoke, array $stats)
+    {
+        $baseStatsSum = 0;
+
+        foreach ($stats as $key => $stat) {
+            $baseStatsSum += $stat['base_stat'];
+        }
+
+        return [$nomePoke => $baseStatsSum];
+    }
 
 
     public function getDataPoke(string $nomePoke)
@@ -79,42 +76,5 @@ public function getAttack(string $nomePoke, array $stats)
             ]
         );
         return json_decode($firstPoke->getBody(), true);
-    }
-
-
-
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
