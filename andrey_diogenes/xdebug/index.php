a230ce8c2d0ae0a939baef282fc4745ec5f60dba
andreydiogenes <dedeidiogenes@hotmail.com>
2022-07-27 21:05:34 -0300

    Projeto com Xdebug

---
+++ andrey_diogenes/xdebug/index.php
@@ -0,0 +1,18 @@
+<?php
+
+use Unialfa\Phpxdebug\Adapters\Cliente;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$cliente = new Cliente();
+
+echo $cliente->hello();
+
+/*
+$arrayTeste = [
+    "Teste" => "aa"s
+]
+
+var_dump($arrayTeste);exit;
+
+echo phpinfo();*/
