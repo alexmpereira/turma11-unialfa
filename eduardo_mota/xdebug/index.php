146e933e17debac8ff3375f6af031f90770b045f
Dumota <edumota02@gmail.com>
2022-07-27 21:04:05 -0300

    inciando projeto

---
+++ eduardo_mota/xdebug/index.php
@@ -0,0 +1,18 @@
+<?php
+
+use Unialfa\Phpxdebug\Adapters\Cliente;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$cliente = new Cliente();
+
+echo $cliente->hello();
+
+// $array = [
+//     "teste" => "as0"2
+// ];
+
+
+// var_dump($array);
+// exit;
+
