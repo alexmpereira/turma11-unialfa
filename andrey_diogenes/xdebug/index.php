be12464ed938660bc6133dc8748d2bd60e4b93fe
andreydiogenes <dedeidiogenes@hotmail.com>
2022-07-27 22:04:32 -0300

    Alterações na Adapter e na Domain

--- andrey_diogenes/xdebug/index.php
+++ andrey_diogenes/xdebug/index.php
@@ -4,9 +4,16 @@ use Unialfa\Phpxdebug\Adapters\Cliente;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$cliente = new Cliente();
+$mockCliente = [
+    "nome" => "Teste",
+    "idade" => 32,
+    "email" => "teste@gmail.com",
+    "cpf" => "12345678910"
+];
 
-echo $cliente->hello();
+$cliente = new Cliente($mockCliente);
+
+echo json_encode($cliente->imprimeDados());
 
 /*
 $arrayTeste = [
