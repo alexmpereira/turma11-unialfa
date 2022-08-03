f3af83b9828c67602e3dc00893a55da08ee49184
joas-vieira <joas.vieira.dev@gmail.com>
2022-08-03 20:28:09 -0300

    feat: add endereco domain

--- joas_vieira/xdebug/src/Adapters/Cliente.php
+++ joas_vieira/xdebug/src/Adapters/Cliente.php
@@ -3,15 +3,16 @@
 namespace Unialfa\PhpXDebug\Adapters;
 
 use Unialfa\PhpXDebug\Domain\Cliente as DomainCliente;
+use Unialfa\PhpXDebug\Domain\Endereco;
 
 class Cliente
 {
     protected DomainCliente $entity;
 
-    public function __construct(array $cliente)
+    public function __construct(array $cliente, Endereco $endereco)
     {
         $this->entity = new DomainCliente();
-        $this->entity->populaCliente($cliente);
+        $this->entity->populaCliente($cliente, $endereco);
     }
 
     public function imprimeDados()
