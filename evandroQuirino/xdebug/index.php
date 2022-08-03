01793e1e4005d7fa2004689e40856ae6163519a9
evandroquirino <evandroquirin@gmail.com>
2022-08-03 19:35:37 -0300

    inicia projeto xdebug

--- evandroQuirino/xdebug/index.php
+++ evandroQuirino/xdebug/index.php
@@ -1,13 +1,25 @@
-<?php
+<?php 
+// ini_set('display_errors', 1);
 
-use Unialfa\Phpxdebug\Adapters\Cliente;
+use Unialfa\PhpXDebug\Adapters\Cliente;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$arrayTeste =[
-    "Teste" => "aa"
+
+$mockCliente = [
+    "nome" => "João",
+    "email" => "teste@gmail.com",
+    "idade" => "20",
+    "cpf" => "123456789"
 ];
 
-$cliente = new Cliente();
 
-echo $cliente ->hello();
+
+$cliente = new Cliente($mockCliente);
+
+echo json_encode($cliente->imprimeDados());
+
+
+// var_dump($arrayTeste);exit;
+
+// echo phpinfo();
--- evandroQuirino/xdebug/src/Adapters/Cliente.php
+++ evandroQuirino/xdebug/src/Adapters/Cliente.php
@@ -1,10 +1,20 @@
 <?php
-namespace Unialfa\Phpxdebug\Adapters;
 
+namespace Unialfa\PhpXDebug\Adapters;
+
+use Unialfa\PhpXDebug\Domain\Cliente as DomainCliente;
 class Cliente
 {
-    public function hello()
+    protected DomainCliente $entity;
+
+    public function __construct(array $cliente)
     {
-        return "ola mundo!!";
+        $this->entity = new DomainCliente();
+        $this->entity->populaCliente($cliente);
+    }
+
+    public function imprimeDados()
+    {
+        return $this->entity->imprimeDadosCliente();
     }
 }
