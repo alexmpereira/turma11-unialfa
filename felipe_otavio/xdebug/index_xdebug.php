c358ec3bbf1949951d5a3aa3d6320339783614bf
FelipeOtavio <felipecheronn@gmail.com>
2022-08-03 22:05:04 -0300

    carro ligado

---
+++ felipe_otavio/xdebug/index_xdebug.php
@@ -0,0 +1,24 @@
+<?php
+
+use Unialfa\Phpxdebug\Adapters\Cliente;
+use Unialfa\Phpxdebug\Domain\Endereco;
+
+require_once __DIR__ .'/vendor/autoload.php';
+
+$mockCliente = [
+    "nome" => "Teste",
+    "idade" => 32,
+    "email" => "teste@gmail.com",
+    "cpf" => "12345678910"
+];
+
+$mockEndereco = [
+    "endereco" => "Rua Unialfa",
+    "cidade" => "Umuaramra",
+    "numero" => 1004
+];
+
+$endereco = new Endereco($mockEndereco);
+$cliente = new Cliente($mockCliente, $endereco);
+
+echo json_encode($cliente->imprimeDados());
