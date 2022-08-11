1664b191f1ac9119eb3f0c01c1a5e3f1d43115e7
FelipeOtavio <felipecheronn@gmail.com>
2022-08-10 21:31:53 -0300

    solid

---
+++ felipe_otavio/solid/SingleResponsibility/Infra/Mailer.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Mailer implements Email 
+{
+    private $mail;
+
+    public function __construct()
+    {
+        //Instancia da library PHPMailer
+        //Config do email (host, username, password)
+    }
+    public function sendEmail(
+    string $assunto,
+    string $conteudo,
+    array $destinatario,
+    array $copiarPara = []
+    ): bool
+    {
+        return true;
+    }
+}
