36ae38b8a499dc6b5a82cf3c9fcb4dcc795f4304
BrN-Gabs <brunogabriel58@outlook.com>
2022-08-17 20:11:09 -0300

    Open-Closed Principle

---
+++ brunogabriel/solid/OpenClosed2.php
@@ -0,0 +1,33 @@
+<?php
+
+interface Trabalhavel {
+    public function work();
+}
+// Open Closed Principle Violation
+class Programador implements Trabalhavel
+{
+    public function work()
+    {
+        return 'coding';
+    }
+}
+
+class Tester implements Trabalhavel
+{
+    public function work()
+    {
+        return 'testing';
+    }
+}
+
+class GerenciamentoProjeto 
+{
+    public function process(Trabalhavel $member)
+    {   
+        return $member->work();
+    }
+}
+
+$progamador = new Programador();
+$gerenciamento = new GerenciamentoProjeto();
+echo $gerenciamento->process($progamador);
