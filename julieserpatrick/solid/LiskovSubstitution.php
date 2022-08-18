5169a315573fa54ffbb18210fe6f7fdcdef6a15f
julieserpatrick <julieser_angel@hotmail.com>
2022-08-17 21:54:12 -0300

    Liskov

---
+++ julieserpatrick/solid/LiskovSubstitution.php
@@ -0,0 +1,24 @@
+<?php
+
+interface HabilidadeDeVoar extends
+{
+    public function voar();
+}
+
+class Passaro
+{
+    
+}
+
+class Pombo extends Passaro implements HabilidadeDeVoar
+{
+    public function voar()
+    {
+
+    }
+}
+
+class Avestruz extends Passaro
+{
+
+}
